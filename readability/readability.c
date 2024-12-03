c9364f737116dc343dbf70bacbfdf1da4dd538f7
olili27 <timmolili27@gmail.com>
2024-12-03 08:09:37 +0300

    add readability

--- readability/readability.c
+++ readability/readability.c
@@ -1,5 +1,5 @@
-#include <ctype.h>
 #include <cs50.h>
+#include <ctype.h>
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
@@ -18,22 +18,74 @@ int main(void)
     int words = count_words(text);
     int sentences = count_sentences(text);
 
+    printf("%i, %i, %i\n", letters, words, sentences);
+    float L = ((float)letters / (float)words) * 100;
+    float S = ((float)sentences / (float)words) * 100;
+    printf("%f, %f, %f\n", L, S, round(0.0588 * L - 0.296 * S - 15.8));
+
     // Compute the Coleman-Liau index
+    int index = (int) (round(0.0588 * L - 0.296 * S - 15.8));
 
     // Print the grade level
+    if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
 }
 
 int count_letters(string text)
 {
     // Return the number of letters in text
+    int count = 0;
+
+    for(int i = 0, len = strlen(text); i < len; i++)
+    {
+        if(isalpha(text[i]))
+        {
+            count++;
+        }
+    }
+
+    return count;
 }
 
 int count_words(string text)
 {
     // Return the number of words in text
+    int count = 1;
+
+    for (int i = 0, len = strlen(text); i < len; i++)
+    {
+        if (text[i] == ' ')
+        {
+            count++;
+        }
+    }
+
+    return count;
 }
 
 int count_sentences(string text)
 {
     // Return the number of sentences in text
+    int count = 0;
+
+    for (int i = 0, len = strlen(text); i < len; i++)
+    {
+        int letter = text[i];
+        if (letter == '.' || letter == '!' || letter == '?')
+        {
+            count++;
+        }
+    }
+
+    return count;
 }
---
+++ test.c
@@ -0,0 +1,7 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%i", (int)round(123.675867));
+}
